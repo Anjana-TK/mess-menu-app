12b7f60fc3d94633c35885f80403b524dd14e5fe
Anjana-TK <115389489+Anjana-TK@users.noreply.github.com>
2024-03-09 15:36:09 +0530

    Added Loading and Styled Upload Page

--- client/src/components/DisplayPage.jsx
+++ client/src/components/DisplayPage.jsx
@@ -1,16 +1,16 @@
-// DisplayPage.js
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const DisplayPage = () => {
   const [menuImageUrl, setMenuImageUrl] = useState('')
+  const [isLoading, setIsLoading] = useState(false) // Add a loading state
 
   useEffect(() => {
-    // Fetch today's dinner photo on component mount
     fetchMenu()
   }, [])
 
   const fetchMenu = async () => {
+    setIsLoading(true) // Set loading to true before the request
     try {
       const response = await axios.get(
         'https://mess-menu-app.onrender.com/api/menu'
@@ -18,6 +18,8 @@ const DisplayPage = () => {
       setMenuImageUrl(response.data)
     } catch (error) {
       console.error(error)
+    } finally {
+      setIsLoading(false) // Set loading to false after the request
     }
   }
 
@@ -26,7 +28,9 @@ const DisplayPage = () => {
       <div className="bg-gray-200 p-8">
         <h2 className="text-2xl font-bold mb-4">Today's Dinner Photo</h2>
         <div className="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
-          {menuImageUrl ? (
+          {isLoading ? (
+            <p>Loading menu...</p> // Display loading message
+          ) : menuImageUrl ? (
             <img
               src={`data:image/jpeg;base64,${menuImageUrl}`}
               alt="Today's Dinner"
--- client/src/components/UploadPage.jsx
+++ client/src/components/UploadPage.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 
 const UploadPage = () => {
   const [selectedFile, setSelectedFile] = useState(null)
-  const navigate = useNavigate() // Use useNavigate instead of useHistory
+  const navigate = useNavigate()
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0])
@@ -35,10 +35,20 @@ const UploadPage = () => {
   }
 
   return (
-    <div>
-      <h2>Upload Menu Photo</h2>
-      <input type="file" accept="image/*" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
+    <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded-md shadow-md">
+      <h2 className="text-2xl font-bold mb-4">Upload Menu Photo</h2>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleFileChange}
+        className="mb-4"
+      />
+      <button
+        onClick={handleUpload}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Upload
+      </button>
     </div>
   )
 }
